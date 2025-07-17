69ac289f8cdade5e020b65a9acc9d28d914fb1e9
yilong877 <yilong877@gmail.com>
2025-07-17 08:10:44 +0800

    Just a test

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# fcitx5-im-toogle.nvim
+# fcitx5-im-toggle.nvim
+
+A minimal Neovim plugin to automatically toggle `fcitx5` input method:
+
+- When entering insert mode: restores previous IME state (e.g. Chinese)
+- When leaving insert mode: switches to English input (`fcitx5-remote -c`)
+
+## Requirements
+
+- `fcitx5`
+- `fcitx5-remote` in PATH
+
+## Installation (with lazy.nvim)
+
+```lua
+{
+  "yourgithubname/fcitx5-im-toggle.nvim",
+  config = function()
+    require("fcitx5_im_toggle").setup()
+  end
+}
+
---
+++ init.lua
@@ -0,0 +1,44 @@
+local M = {}
+
+local last_ime_state = 0
+
+local function has_fcitx5()
+  return vim.fn.executable("fcitx5-remote") == 1
+end
+
+local function get_ime_state()
+  if not has_fcitx5() then return 0 end
+  return tonumber(vim.fn.system("fcitx5-remote")) or 0
+end
+
+local function ime_off()
+  if has_fcitx5() then
+    vim.fn.system("fcitx5-remote -c")
+  end
+end
+
+local function ime_on()
+  if has_fcitx5() then
+    vim.fn.system("fcitx5-remote -o")
+  end
+end
+
+function M.setup()
+  vim.api.nvim_create_autocmd("InsertLeave", {
+    callback = function()
+      last_ime_state = get_ime_state()
+      ime_off()
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("InsertEnter", {
+    callback = function()
+      if last_ime_state ~= 0 then
+        ime_on()
+      end
+    end,
+  })
+end
+
+return M
+
